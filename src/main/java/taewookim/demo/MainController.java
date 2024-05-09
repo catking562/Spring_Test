b814cdd0fd9891143cfb2b99afd7bbfcdbba08ff
catking562 <taewookim562@gmail.com>
2024-05-09 17:32:16 +0900

    저장(json 파일 보내기가 잘 안되는중)

---
+++ src/main/java/taewookim/demo/MainController.java
@@ -0,0 +1,23 @@
+package taewookim.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam(name = "name", required = false, defaultValue = "김태우") String name, Model model) {
+        model.addAttribute("name", name);
+        return "introduceme";
+    }
+
+    @GetMapping("/json")
+    public String json() {
+        return "test";
+    }
+
+}
