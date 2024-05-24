83427a30a6c9c9bcb9a508a884d11800474efd0e
BuildTools <unconfigured@null.spigotmc.org>
2024-05-24 16:57:37 +0900

    세이브

---
+++ src/main/java/taewookim/demo/controller/ViewController.java
@@ -0,0 +1,20 @@
+package taewookim.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    public ViewController() {
+
+    }
+
+    @GetMapping("/posts")
+    public String getViewArticles(Model model) {
+        model.addAttribute()
+        return "viewarticles";
+    }
+
+}
